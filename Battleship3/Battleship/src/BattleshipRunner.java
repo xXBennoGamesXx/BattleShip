c134ae044360a465acad88fa62778bcb81bc4a0e
Absguayaco <91566439+abodero22@users.noreply.github.com>
2021-12-10 09:42:54 -0500

    Add files via upload
    Worked on class of December 8th

---
+++ Battleship3/Battleship/src/BattleshipRunner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BattleshipRunner {
+    public static void main(String args[]){
+
+
+        int choice;
+        Board gameBoard = new Board();
+        Game game = new Game();
+        gameBoard.displayBoard();
+        //Game.initializeGame();
+        game.playBattleship1(gameBoard);
+        //gameBoard.placeShip(4, true, 2, 2);
+
+
+    }
+
+
+}
---
+++ Battleship3/Battleship/src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    String name;
+    boolean conditionWin = false;
+
+    public Player(String n){
+        this.name = n;
+    }
+
+    public String getPlayerName(){
+        return this.name;
+    }
+
+
+}
